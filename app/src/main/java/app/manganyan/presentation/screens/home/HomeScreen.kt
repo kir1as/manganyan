8be1a775540b3f95c32b854cd9c64a6c2781dcf5
Theo Gmz <theogamiz31@gmail.com>
2023-07-27 17:14:20 +0200

    category/searchbar

--- app/src/main/java/app/manganyan/presentation/screens/home/HomeScreen.kt
+++ app/src/main/java/app/manganyan/presentation/screens/home/HomeScreen.kt
@@ -57,60 +57,74 @@ fun HomeScreen(viewModel: HomeViewModel = hiltViewModel(), navController: NavHos
 
 @Composable
 fun MangaCard(mangaTitle: String?, mangaId: String?, mangaCover: String?, mangaDesc: String?, navController: NavHostController) {
-    Card(
-        modifier = Modifier
-            .padding(16.dp)
-            .fillMaxWidth()
-            .clickable {
-                mangaId?.let {
-                    navController.navigate(Screens.MangaDetailScreen.route + "/${mangaId}")
+    Column {
+        Spacer(modifier = Modifier.weight(1f)) // Spacer to center "Bienvenue" vertically
+        Text(
+            text = "Bienvenue",
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Bold,
+            color = MaterialTheme.colors.primary,
+            maxLines = 1,
+            modifier = Modifier.padding(20.dp) // Padding of 20
+        )
+        app.manganyan.presentation.screens.search.SearchBar(onChange = {
+            viewModel.onSearchChanged(it)
+        })
+        Card(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth()
+                .clickable {
+                    mangaId?.let {
+                        navController.navigate(Screens.MangaDetailScreen.route + "/${mangaId}")
+                    }
                 }
-            }
-    ) {
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            verticalAlignment = Alignment.CenterVertically
         ) {
-            if (mangaCover != null && mangaId != null) {
-                val imageUrl = "https://mangadex.org/covers/$mangaId/$mangaCover"
-                Image(
-                    painter = rememberAsyncImagePainter(imageUrl),
-                    contentDescription = null,
-                    modifier = Modifier
-                        .height(150.dp)
-                        .width(100.dp)
-                        .aspectRatio(2 / 3f) // To maintain the aspect ratio of the image
-                )
-            } else {
-                Image(
-                    painter = painterResource(R.drawable.qrcode), // Replace with your desired placeholder image
-                    contentDescription = null,
-                    modifier = Modifier
-                        .height(150.dp)
-                        .width(100.dp)
-                        .aspectRatio(2 / 3f) // To maintain the aspect ratio of the image
-                )
-            }
-
-            Column(
-                modifier = Modifier
-                    .padding(start = 16.dp)
-                    .weight(1f) // To make the title and description take up the remaining space
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
             ) {
-                Text(
-                    text = mangaTitle ?: "Unknown Title",
-                    fontSize = 18.sp,
-                    fontWeight = FontWeight.Bold,
-                    color = MaterialTheme.colors.primary
-                )
-                Spacer(modifier = Modifier.height(8.dp))
-                if (mangaDesc != null) {
-                    Text(
-                        text = mangaDesc, // Replace this with the actual description text
-                        fontSize = 14.sp,
-                        color = MaterialTheme.colors.secondary,
-                        maxLines = 2 // Limit the description to two lines
+                if (mangaCover != null && mangaId != null) {
+                    val imageUrl = "https://mangadex.org/covers/$mangaId/$mangaCover"
+                    Image(
+                        painter = rememberAsyncImagePainter(imageUrl),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .height(150.dp)
+                            .width(100.dp)
+                            .aspectRatio(2 / 3f) // To maintain the aspect ratio of the image
                     )
+                } else {
+                    Image(
+                        painter = painterResource(R.drawable.qrcode), // Replace with your desired placeholder image
+                        contentDescription = null,
+                        modifier = Modifier
+                            .height(150.dp)
+                            .width(100.dp)
+                            .aspectRatio(2 / 3f) // To maintain the aspect ratio of the image
+                    )
+                }
+
+                Column(
+                    modifier = Modifier
+                        .padding(start = 16.dp)
+                        .weight(1f) // To make the title and description take up the remaining space
+                ) {
+                    Text(
+                        text = mangaTitle ?: "Unknown Title",
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = MaterialTheme.colors.primary
+                    )
+                    Spacer(modifier = Modifier.height(8.dp))
+                    if (mangaDesc != null) {
+                        Text(
+                            text = mangaDesc, // Replace this with the actual description text
+                            fontSize = 14.sp,
+                            color = MaterialTheme.colors.secondary,
+                            maxLines = 2 // Limit the description to two lines
+                        )
+                    }
                 }
             }
         }
