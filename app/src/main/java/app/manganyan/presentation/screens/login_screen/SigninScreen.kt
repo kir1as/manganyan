d516c790edcc08cadf636571b234d7ea32264495
Thomas Southasa <thomas.southasa@gmail.com>
2023-07-27 16:15:03 +0200

    auth/design_login

--- app/src/main/java/app/manganyan/presentation/screens/login_screen/SigninScreen.kt
+++ app/src/main/java/app/manganyan/presentation/screens/login_screen/SigninScreen.kt
@@ -3,6 +3,7 @@ package app.manganyan.presentation.screens.login_screen
 import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
@@ -11,26 +12,20 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.rememberNavController
-import app.manganyan.R
 import app.manganyan.presentation.navigation.Screens
 import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.coroutines.launch
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import app.manganyan.presentation.screens.login_screen.SignInScreen
-import app.manganyan.presentation.screens.signup_screen.SignUpScreen
+import app.manganyan.presentation.ui.theme.BlueYellowGradient
 
 @Composable
 fun SignInScreen(
@@ -40,10 +35,6 @@ fun SignInScreen(
 ) {
 
     val googleSignInState = viewModel.googleState.value
-    val navController1 = rememberNavController()
-
-
-
 
     val launcher =
         rememberLauncherForActivityResult(contract = ActivityResultContracts.StartActivityForResult()) {
@@ -65,170 +56,163 @@ fun SignInScreen(
     val state = viewModel.signInState.collectAsState(initial = null)
 
 
+    val cardHeight =
+        with(LocalDensity.current) { LocalConfiguration.current.screenHeightDp.dp * 2 / 5 }
 
-
-    Column(
+    Box(
         modifier = Modifier
-            .fillMaxSize()
-            .padding(start = 30.dp, end = 30.dp),
-        verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
     ) {
-        Text(
-            text = "Enter your credential's to register",
-            fontWeight = FontWeight.Medium,
-            fontSize = 15.sp,
-            color = Color.Gray,
-            //fontFamily = RegularFont
-        )
-        TextField(
-            value = email,
-            onValueChange = {
-                email = it
-            },
-            modifier = Modifier.fillMaxWidth(),
-            colors = TextFieldDefaults.textFieldColors(
-                backgroundColor = Color.Cyan,
-                cursorColor = Color.Black,
-                disabledLabelColor = Color.Cyan, unfocusedIndicatorColor = Color.Transparent,
-                focusedIndicatorColor = Color.Transparent
-            ), shape = RoundedCornerShape(8.dp), singleLine = true, placeholder = {
-                Text(text = "Email")
-            }
-        )
-        Spacer(modifier = Modifier.height(16.dp))
-        TextField(
-            value = password,
-            onValueChange = {
-                password = it
-            },
-            modifier = Modifier.fillMaxWidth(),
-            colors = TextFieldDefaults.textFieldColors(
-                backgroundColor = Color.Cyan,
-                cursorColor = Color.Black,
-                disabledLabelColor = Color.Cyan, unfocusedIndicatorColor = Color.Transparent,
-                focusedIndicatorColor = Color.Transparent
-            ), shape = RoundedCornerShape(8.dp), singleLine = true, placeholder = {
-                Text(text = "Password")
-            }
-        )
-
-        Button(
-            onClick = {
-                scope.launch {
-                    viewModel.loginUser(email, password)
-                }
-            },
+        Column(
+            modifier = Modifier.align(Alignment.TopCenter).padding(top = 30.dp)
+        ) {
+            Text(
+                text = "Manganyan",
+                fontWeight = FontWeight.Bold,
+                fontSize = 30.sp,
+                color = Color.Black,
+                modifier = Modifier.padding(vertical = 16.dp)
+            )
+        }
+        Card(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(top = 20.dp, start = 30.dp, end = 30.dp),
-            colors = ButtonDefaults.buttonColors(
-                backgroundColor = Color.Black, contentColor = Color.White
-            ),
-            shape = RoundedCornerShape(15.dp)
+                .height(cardHeight)
+                .align(Alignment.BottomCenter)
+                .height(cardHeight),
+            shape = RoundedCornerShape(28.dp),
+            elevation = 4.dp
         ) {
-            Text(text = "Sign In", color = Color.White, modifier = Modifier.padding(7.dp))
-        }
-        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
-            if (state.value?.isLoading == true) {
-                CircularProgressIndicator()
-            }
-
-        }
-
-
-        Button(
-            onClick = {
-                navController.navigate(Screens.SignUpScreen.route)
-
-            },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(top = 20.dp, start = 30.dp, end = 30.dp),
-            colors = ButtonDefaults.buttonColors(
-                backgroundColor = Color.Black, contentColor = Color.White
-            ),
-            shape = RoundedCornerShape(15.dp)
-        ) {
-            Text(text = "Sign Up", color = Color.White, modifier = Modifier.padding(7.dp))
-        }
-
-
-        Text(
-            text = "New User? Sign Up ",
-            fontWeight = FontWeight.Bold,
-            color = Color.Black,
-            //fontFamily = RegularFont
-        )
-        Text(text = "or connect with", fontWeight = FontWeight.Medium, color = Color.Gray)
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(top = 10.dp),
-            horizontalArrangement = Arrangement.Center
-        ) {
-            IconButton(onClick = {
-                val gso= GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                    .requestEmail()
-                    .requestIdToken("")
-                    .build()
-
-                val googleSingInClient = GoogleSignIn.getClient(context, gso)
-
-                launcher.launch(googleSingInClient.signInIntent)
-
-            }) {
-                Icon(
-                    painter = painterResource(id = R.drawable.ic_launcher_foreground),
-                    contentDescription = "Google Icon",
-                    modifier = Modifier.size(50.dp),
-                    tint = Color.Unspecified
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(BlueYellowGradient),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                TextField(
+                    value = email,
+                    onValueChange = {
+                        email = it
+                    },
+                    colors = TextFieldDefaults.textFieldColors(
+                        backgroundColor = Color.Transparent,
+                        cursorColor = Color.Black,
+                        textColor = Color.Black,
+                        unfocusedIndicatorColor = Color.Transparent,
+                        focusedIndicatorColor = Color.Transparent
+                    ), shape = RoundedCornerShape(8.dp), singleLine = true, placeholder = {
+                        Text(text = "Email")
+                    }
                 )
-            }
-            Spacer(modifier = Modifier.width(20.dp))
-            IconButton(onClick = {
-
-            }) {
-                Icon(
-                    painter = painterResource(id = R.drawable.ic_launcher_background),
-                    contentDescription = "Facebook Icon",
-                    modifier = Modifier.size(50.dp),
-                    tint = Color.Unspecified
+                Divider(
+                    color = Color.Black,
+                    thickness = 1.dp,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(start = 30.dp, end = 30.dp),
                 )
-            }
-            LaunchedEffect(key1 = state.value?.isSuccess) {
-                scope.launch {
-                    if (state.value?.isSuccess?.isNotEmpty() == true) {
-                        val success = state.value?.isSuccess
-                        Toast.makeText(context, "${success}", Toast.LENGTH_LONG).show()
-                        navController.navigate(Screens.MainScreen.route)
+                TextField(
+                    value = password,
+                    onValueChange = {
+                        password = it
+                    },
+                    colors = TextFieldDefaults.textFieldColors(
+                        backgroundColor = Color.Transparent,
+                        cursorColor = Color.Black,
+                        textColor = Color.Black,
+                        unfocusedIndicatorColor = Color.Transparent,
+                        focusedIndicatorColor = Color.Transparent
+                    ), shape = RoundedCornerShape(8.dp), singleLine = true, placeholder = {
+                        Text(text = "Password")
+                    }
+                )
+                Divider(
+                    color = Color.Black,
+                    thickness = 1.dp,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(start = 30.dp, end = 30.dp),
+                )
+                Button(
+                    onClick = {
+                        scope.launch {
+                            viewModel.loginUser(email, password)
+                        }
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 20.dp, start = 30.dp, end = 30.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        backgroundColor = Color.White, contentColor = Color.Black
+                    ),
+                    shape = RoundedCornerShape(15.dp)
+                ) {
+                    Text(text = "Continue", color = Color.Black, modifier = Modifier.padding(7.dp))
+                }
+                TextButton(
+                    onClick = {
+                        navController.navigate(Screens.SignUpScreen.route)
+                    },
+                    modifier = Modifier
+                        .align(Alignment.End)
+                        .padding(bottom = 14.dp)
+                ) {
+                    Text(text = "No account? Register", color = Color.White)
+                }
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    if (state.value?.isLoading == true) {
+                        CircularProgressIndicator()
+                    }
+
+                }
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 10.dp),
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    LaunchedEffect(key1 = state.value?.isSuccess) {
+                        scope.launch {
+                            if (state.value?.isSuccess?.isNotEmpty() == true) {
+                                val success = state.value?.isSuccess
+                                Toast.makeText(context, "${success}", Toast.LENGTH_LONG).show()
+                                navController.navigate(Screens.MainScreen.route)
+                            }
+                        }
+                    }
+
+                    LaunchedEffect(key1 = state.value?.isError) {
+                        scope.launch {
+                            if (state.value?.isError?.isNotEmpty() == true) {
+                                val error = state.value?.isError
+                                Toast.makeText(context, "${error}", Toast.LENGTH_LONG).show()
+                            }
+                        }
+                    }
+
+                    LaunchedEffect(key1 = googleSignInState.success) {
+                        scope.launch {
+                            if (googleSignInState.success != null) {
+                                Toast.makeText(context, "Sign In Success", Toast.LENGTH_LONG).show()
+                            }
+                        }
+                    }
+
+                }
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    if (googleSignInState.loading) {
+                        CircularProgressIndicator()
                     }
                 }
             }
-
-            LaunchedEffect(key1 = state.value?.isError) {
-                scope.launch {
-                    if (state.value?.isError?.isNotEmpty() == true) {
-                        val error = state.value?.isError
-                        Toast.makeText(context, "${error}", Toast.LENGTH_LONG).show()
-                    }
-                }
-            }
-
-            LaunchedEffect(key1 = googleSignInState.success) {
-                scope.launch {
-                    if (googleSignInState.success != null) {
-                        Toast.makeText(context, "Sign In Success", Toast.LENGTH_LONG).show()
-                    }
-                }
-            }
-
         }
-        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
-            if (googleSignInState.loading){
-                CircularProgressIndicator()
-            }
-        }
-
-
     }
+
 }
