0fd15795875e3e19a789a511c6857ce37005d02e
Thomas Southasa <thomas.southasa@gmail.com>
2023-07-27 16:16:34 +0200

    auth/design_register

--- app/src/main/java/app/manganyan/presentation/screens/signup_screen/SignUpScreen.kt
+++ app/src/main/java/app/manganyan/presentation/screens/signup_screen/SignUpScreen.kt
@@ -1,6 +1,7 @@
 package app.manganyan.presentation.screens.signup_screen
 
 import android.widget.Toast
+import androidx.compose.foundation.background
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -10,9 +11,9 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -30,22 +31,19 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.TextField
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.material.Button
-import androidx.compose.material.IconButton
+import androidx.compose.material.Card
+import androidx.compose.material.Divider
+import androidx.compose.material.TextButton
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.Icon
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalDensity
 
 
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.rememberNavController
-import app.manganyan.R
 import app.manganyan.presentation.navigation.Screens
-import app.manganyan.presentation.ui.theme.FirstActivityTheme
+import app.manganyan.presentation.ui.theme.BlueYellowGradient
 import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -63,113 +61,145 @@ fun SignUpScreen(
     var email by rememberSaveable { mutableStateOf("") }
     var password by rememberSaveable { mutableStateOf("") }
 
+    val cardHeight =
+        with(LocalDensity.current) { LocalConfiguration.current.screenHeightDp.dp * 2 / 5 }
 
-    Column(modifier = Modifier
-        .fillMaxSize()
-        .padding(start = 30.dp, end = 30.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+    Box(
+        modifier = Modifier
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
     ) {
-        Text(
-            text = "Enter your credential's to register",
-            fontWeight = FontWeight.Medium,
-            fontSize = 15.sp,
-            color = Color.Gray,
-            //fontFamily = RegularFont
-        )
-        TextField(
-            value= email,
-            onValueChange = {
-                email = it
-        },
-            modifier = Modifier.fillMaxWidth(),
-            colors = TextFieldDefaults.textFieldColors(backgroundColor = Color.Cyan, cursorColor = Color.Black),
-            shape = RoundedCornerShape(8.dp), singleLine = true, placeholder = {
-                Text(text = "Email")
-            }
-        //
-        )
-        Spacer(modifier = Modifier.height(16.dp))
+        Column(
+            modifier = Modifier
+                .align(Alignment.TopCenter)
+                .padding(top = 30.dp)
+        ) {
+            Text(
+                text = "Manganyan",
+                fontWeight = FontWeight.Bold,
+                fontSize = 30.sp,
+                color = Color.Black,
+                modifier = Modifier.padding(vertical = 16.dp),
+            )
+        }
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(cardHeight)
+                .align(Alignment.BottomCenter)
+                .height(cardHeight),
+            shape = RoundedCornerShape(28.dp),
+            elevation = 4.dp
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(BlueYellowGradient),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                TextField(
+                    value = email,
+                    onValueChange = {
+                        email = it
+                    },
+                    colors = TextFieldDefaults.textFieldColors(
+                        backgroundColor = Color.Transparent,
+                        cursorColor = Color.Black,
+                        textColor = Color.Black,
+                        unfocusedIndicatorColor = Color.Transparent,
+                        focusedIndicatorColor = Color.Transparent
+                    ),
+                    shape = RoundedCornerShape(8.dp), singleLine = true, placeholder = {
+                        Text(text = "Email")
+                    }
+                )
+                Divider(
+                    color = Color.Black,
+                    thickness = 1.dp,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(start = 30.dp, end = 30.dp),
+                )
+                TextField(
+                    value = password,
+                    onValueChange = {
+                        password = it
+                    },
+                    colors = TextFieldDefaults.textFieldColors(
+                        backgroundColor = Color.Transparent,
+                        cursorColor = Color.Black,
+                        textColor = Color.Black,
+                        unfocusedIndicatorColor = Color.Transparent,
+                        focusedIndicatorColor = Color.Transparent
+                    ),
+                    shape = RoundedCornerShape(8.dp), singleLine = true, placeholder = {
+                        Text(text = "Password")
+                    }
+                )
+                Divider(
+                    color = Color.Black,
+                    thickness = 1.dp,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(start = 30.dp, end = 30.dp),
+                )
+                Button(
+                    onClick = {
+                        scope.launch {
+                            viewModel.registerUser(email, password)
+                        }
 
-
-        TextField(
-            value= password,
-            onValueChange = {
-                password = it
-            },
-            modifier = Modifier.fillMaxWidth(),
-            colors = TextFieldDefaults.textFieldColors(backgroundColor = Color.Cyan, cursorColor = Color.Black),
-            shape = RoundedCornerShape(8.dp), singleLine = true, placeholder = {
-                Text(text = "Password")
-            }
-            //
-        )
-
-
-        Button(onClick = {
-                scope.launch {
-                    viewModel.registerUser(email, password)
+                    }, modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 20.dp, start = 30.dp, end = 30.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        backgroundColor = Color.White, contentColor = Color.Black
+                    ),
+                    shape = RoundedCornerShape(15.dp)
+                ) {
+                    Text(text = "Register", color = Color.Black, modifier = Modifier.padding(7.dp))
                 }
-                         
-        }, modifier = Modifier
-            .fillMaxWidth()
-            .padding(top = 20.dp, start = 30.dp, end = 30.dp),
-            colors = ButtonDefaults.buttonColors(
-            backgroundColor = Color.Black, contentColor = Color.White
-            ), shape = RoundedCornerShape(15.dp) ) {
-            Text(text = "Sign Up", color = Color.White, modifier = Modifier.padding(7.dp))
-
-        }
-        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
-            if (state.value?.isLoading == true){
-                CircularProgressIndicator()
-            }
-            
-        }
-        Text(text = "Already have an Account? SignIn", fontWeight = FontWeight.Bold, color = Color.Black)
-        Text(text = "Or connect with", fontWeight = FontWeight.Medium, color = Color.Gray)
-        Row(modifier = Modifier
-            .fillMaxWidth()
-            .padding(top = 10.dp), horizontalArrangement = Arrangement.Center) {
-            IconButton(onClick = { /*TODO*/ }) {
-                Icon(painter = painterResource(id = R.drawable.ic_launcher_foreground), contentDescription = "Google Icon", modifier = Modifier.size(50.dp), tint = Color.Unspecified )
-                
-            }
-            
-        }
-        Spacer(modifier = Modifier.width(20.dp))
-        IconButton(onClick = { /*TODO*/ }) {
-            Icon(painter = painterResource(id = R.drawable.ic_launcher_background), contentDescription = "Facebook Icon", modifier = Modifier.size(50.dp), tint = Color.Unspecified )
-
-        }
-
-
-        LaunchedEffect(key1 = state.value?.isSuccess){
-            scope.launch {
-                if(state.value?.isSuccess?.isNotEmpty() == true){
-                    val success = state.value?.isSuccess
-                    Toast.makeText(context, "${success}", Toast.LENGTH_LONG).show()
-                    navController.navigate(Screens.HomeScreen.route)
-
+                TextButton(
+                    onClick = {
+                        navController.navigate(Screens.SignInScreen.route)
+                    },
+                    modifier = Modifier
+                        .align(Alignment.End)
+                        .padding(bottom = 14.dp)
+                ) {
+                    Text(text = "Have account? Sign In", color = Color.White)
                 }
-            }
-        }
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    if (state.value?.isLoading == true) {
+                        CircularProgressIndicator()
+                    }
+                }
+
+                LaunchedEffect(key1 = state.value?.isSuccess) {
+                    scope.launch {
+                        if (state.value?.isSuccess?.isNotEmpty() == true) {
+                            val success = state.value?.isSuccess
+                            Toast.makeText(context, "${success}", Toast.LENGTH_LONG).show()
+                            navController.navigate(Screens.HomeScreen.route)
+                        }
+                    }
+                }
 
 
-        LaunchedEffect(key1 = state.value?.isError){
-            scope.launch {
-                if(state.value?.isError?.isNotEmpty() == true){
-                    val error = state.value?.isError
-                    Toast.makeText(context, "${error}", Toast.LENGTH_LONG).show()
-
-
+                LaunchedEffect(key1 = state.value?.isError) {
+                    scope.launch {
+                        if (state.value?.isError?.isNotEmpty() == true) {
+                            val error = state.value?.isError
+                            Toast.makeText(context, "${error}", Toast.LENGTH_LONG).show()
+                        }
+                    }
                 }
             }
         }
     }
-
-
-
 }
 
 
